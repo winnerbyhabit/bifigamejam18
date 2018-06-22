21a1e50a6f8c6e2b1a00aaac6ff6761e171c9710
Nicolas <lilals@yahoo.de>
2018-06-22 20:50:13 +0000

    Add player function

--- move_player.gd
+++ move_player.gd
@@ -12,5 +12,9 @@ func _ready():
 func _process(delta):
 	# Called every frame. Delta is time since last frame.
 	# Update game logic here.
+	move_right()
 	if Input.is_action_pressed("player_hit"):
 		print("pressed space")
+
+func move_right():
+	pass
