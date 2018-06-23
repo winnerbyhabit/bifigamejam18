20911efa07c0c0490a5797669df973fb1d4cb456
Nicolas <lilals@yahoo.de>
2018-06-23 19:20:30 +0000

    New Claw scene (invisible)

--- Interface.gd
+++ Interface.gd
@@ -6,12 +6,11 @@ extends Node
 
 func _ready():
 	# Called every time the node is added to the scene.
-
 	pass
 
 func add_points(points):
 	$Punkte/Counter.text = str(int($Punkte/Counter.text) + points)
-	 
+
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
