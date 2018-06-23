6f04bdbbf68dd3ca6bc67e640ee3987d64ee798a
Nicolas <lilals@yahoo.de>
2018-06-23 18:31:35 +0000

    Spawn bottles in Bosslevel

--- BossLevel.gd
+++ BossLevel.gd
@@ -8,6 +8,8 @@ export var hidden_wall_id = 4
 func _ready():
 	#_ready von Level.gd wird automatisch aufgerufen
 	set_boundarys(level_boundarys)
+	initial_spawn_bottle() # spawn bottles a second time to get more bottles
+
 
 func set_boundarys(boundarys):
 	var y_coord = $TileMap.tile_coordinates.y 
@@ -20,6 +22,7 @@ func set_boundarys(boundarys):
 	#hidden wall dog
 	$TileMap.set_cell(boundarys.x,y_coord+1,2)
 	$TileMap.set_cell(boundarys.y,y_coord+1,2)
+
 
 func _on_enemy_killed():
 	print('enenmy killed')
