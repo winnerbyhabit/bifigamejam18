6c9fa30646a0ee0ffee1fabce453df45b3a7e1d1
Nicolas <lilals@yahoo.de>
2018-06-23 17:47:44 +0000

    Spawn bottles at start

--- BossLevel.gd
+++ BossLevel.gd
@@ -5,10 +5,7 @@ export(Vector2) var level_boundarys = Vector2(-5,5)
 export var hidden_wall_id = 4
 func _ready():
 	#_ready von Level.gd wird automatisch aufgerufen
-	
 	set_boundarys(level_boundarys)
-	
-	pass
 
 func set_boundarys(boundarys):
 	var y_coord = $TileMap.tile_coordinates.y -1
--- Level.gd
+++ Level.gd
@@ -9,14 +9,17 @@ export var bottom_height = 500
 var bottle_timer = 0
 var bottle_space = 1
 
+
 export var bottle_frequency = 2.0
 # change this value to set distance between bottles
 # higher frequency == more bottles
 
+
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	pass
+	initial_spawn_bottle()
+
 
 func _process(delta):
 	#updates map
@@ -29,46 +32,63 @@ func _process(delta):
 		$bg.position.x -= $TileMap.cell_size.x
 		$bg_floor.position.x -= $TileMap.cell_size.x
 	old_player_pos = $Player.position
-	spawn_bottle(delta)
+	loop_spawn_bottle(delta)
 
 
-func spawn_bottle(delta):
+func random_distance():
+	return rand_range(0.5, 1.0 + (5.0 / bottle_frequency))
+
+
+func spawn_single_bottle(pos):
+	# RNG
+	var r = randi() % 5
+	bottle_space = random_distance()
+	# spawn bottle
+	var bottle_scene
+	var bottle_texture
+	match r:
+		0: # spawn Glass
+			bottle_scene = "res://Glass.tscn"
+			bottle_texture = "res://assets/bottles/glass.png"
+		1: # spawn Bottle 1
+			bottle_scene = "res://Bottle.tscn"
+			bottle_texture = "res://assets/bottles/bottle1.png"
+		2: # spawn Bottle 2
+			bottle_scene = "res://Bottle.tscn"
+			bottle_texture = "res://assets/bottles/bottle2.png"
+		3: # spawn Bottle 3
+			bottle_scene = "res://Bottle.tscn"
+			bottle_texture = "res://assets/bottles/bottle3.png"
+		4: # spawn Bottle 4
+			bottle_scene = "res://Bottle.tscn"
+			bottle_texture = "res://assets/bottles/poison_bottle.png"
+	var scene = load(bottle_scene)
+	var bottle = scene.instance()
+	bottle.set_name("Bottle")
+	bottle.get_node("Texture").texture = load(bottle_texture)
+	bottle.set_destroy_height(bottom_height)
+	bottle.position = pos
+	bottle.connect('destroy',self,'_on_destroy_bottle')
+	add_child(bottle)
+	bottle_timer = 0
+
+
+func loop_spawn_bottle(delta):
 	bottle_timer += delta
-	# TODO timer aendern
 	if bottle_timer >= bottle_space:
-		# RNG
-		var r = randi() % 5
-		bottle_space = rand_range(0.5, 1.0 + (5.0 / bottle_frequency))
-		# TODO Wahrscheinlichkeitsverteilung aendern
-		# spawn bottle
-		var bottle_scene
-		var bottle_texture
-		match r:
-			0: # spawn Glass
-				bottle_scene = "res://Glass.tscn"
-				bottle_texture = "res://assets/bottles/glass.png"
-			1: # spawn Bottle 1
-				bottle_scene = "res://Bottle.tscn"
-				bottle_texture = "res://assets/bottles/bottle1.png"
-			2: # spawn Bottle 2
-				bottle_scene = "res://Bottle.tscn"
-				bottle_texture = "res://assets/bottles/bottle2.png"
-			3: # spawn Bottle 3
-				bottle_scene = "res://Bottle.tscn"
-				bottle_texture = "res://assets/bottles/bottle3.png"
-			4: # spawn Bottle 4
-				bottle_scene = "res://Bottle.tscn"
-				bottle_texture = "res://assets/bottles/poison_bottle.png"
-		var scene = load(bottle_scene)
-		var bottle = scene.instance()
-		bottle.set_name("Bottle")
-		bottle.get_node("Texture").texture = load(bottle_texture)
-		bottle.set_destroy_height(bottom_height)
-		bottle.position = $Player.position + Vector2(1000,10)
-		bottle.connect('destroy',self,'_on_destroy_bottle')
-		add_child(bottle)
-		bottle_timer = 0
-		
+		spawn_single_bottle($Player.position + Vector2(1000,10))
+
+
 func _on_destroy_bottle():
 	if not $destroy_bottle.playing:
 		$destroy_bottle.play()
+
+
+func initial_spawn_bottle():
+	print("initial spawn bottles")
+	print($Player.position)
+	var m = $Player.position.x + 1000
+	var i = 0
+	while i <= m:
+		spawn_single_bottle($Player.position + Vector2(i,10))
+		i += int (120.0 * random_distance())
