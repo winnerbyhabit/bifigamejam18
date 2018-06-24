941a03673bdc69e66cd65d437a7f5e648ea48be5
Christoph Heinen <heinen@fachschaft.info>
2018-06-24 17:50:41 +0200

    interface dinge

--- Interface.gd
+++ Interface.gd
@@ -5,30 +5,34 @@ export var heart_size = 32
 signal game_over
 signal game_won
 
-var t = Timer.new()
 
 func _ready():
+	$Cat/lion_sound.play()
 	add_points(0)
 	add_lifes(0)
 	set_max_lifes($lifes/lifebar.max_value)
 	if global.cat_scratches >= 1: # show cat
-		t.set_wait_time(3)
-		t.set_one_shot(true)
-		get_node("Claw").show()
-		self.add_child(t)
-		t.start()
-		yield(t, "timeout")
-		get_node("Cat").hide()
-		get_node("Claw").show()
-		match global.cat_scratches:
-			1:
-				get_node("Claw").texture = load("res://assets/claws/claw1.png")
-			2:
-				get_node("Claw").texture = load("res://assets/claws/claw2.png")
-			3:
-				get_node("Claw").texture = load("res://assets/claws/claw3.png")
-			4:
-				get_node("Claw").texture = load("res://assets/claws/claw4.png")
+		get_node("Cat").show()
+		$Timer.start()
+		$Timer.connect('timeout',self,'ready2')
+
+
+		get_tree().paused = true
+
+		
+func ready2():
+	get_tree().paused = false
+	get_node("Cat").hide()
+	match global.cat_scratches:
+		1:
+			get_node("Claw").texture = load("res://assets/claws/claw1.png")
+		2:
+			get_node("Claw").texture = load("res://assets/claws/claw2.png")
+		3:
+			get_node("Claw").texture = load("res://assets/claws/claw3.png")
+		4:
+			get_node("Claw").texture = load("res://assets/claws/claw4.png")
+	get_node("Claw").show()
 
 func add_points(points):
 	global.points += points
